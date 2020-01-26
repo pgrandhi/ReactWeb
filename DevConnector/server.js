4b93cf60c0eed1c3bab055363ab7e2b1302e9d53
Prema Grandhi <pgrandhi@outlook.com>
2020-01-26 13:37:00 -0800

    Adding initial setup for DevConnector
    Adding User model and user register route as part of initial setup

---
+++ DevConnector/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+const bodyparser = require('body-parser');
+const app = express();
+
+//body parser middleware
+app.use(bodyparser.urlencoded({extended:false}));
+
+app.use(bodyparser.json());
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to mongodb 
+//promise functions
+mongoose
+   .connect(db)
+   .then(() => console.log('MongoDB connected!'))
+   .catch(err => console.log(err));
+
+//first route  - arrow functions / lambdas/delegates
+app.get('/', (reg, res) => res.send('Hello World'));
+
+// User routes
+// /api/users - respective users - users.js
+app.use('/api/users', users);
+
+// /api/profile - profile - profile.js
+app.use('/api/profile', profile);
+
+app.use('/api/posts', posts);
+
+const port = 5555
+//app.listen(port);
+//back ticks are used for inline formatting
+app.listen(port, () => console.log(`Server running on port ${port}`));
